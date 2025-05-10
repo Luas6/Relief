17bd2e9f916911f484ecca77474fc044ddd5396a
Saul Matías Jiménez <saulmaji@gmail.com>
2025-05-10 23:29:16 +0200

    Cambios 10/05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+declare var bootstrap: any;  // Declaramos la variable bootstrap
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,5 +15,11 @@ export class AppComponent {
 
   scrollToContact() {
       document.getElementById('contacto')?.scrollIntoView({ behavior: 'smooth' });
+  }
+  closeNavbar(navbar: HTMLElement): void {
+    if (window.innerWidth < 992 && navbar.classList.contains('show')) {
+      const bsCollapse = bootstrap.Collapse.getInstance(navbar) || new bootstrap.Collapse(navbar);
+      bsCollapse.hide();
+    }
   }
 }
