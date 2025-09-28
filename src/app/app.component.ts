65e1a437ab19284324db6dbc2db81ce153ea6127
Saul Matías Jiménez <saulmaji@gmail.com>
2025-09-28 21:54:24 +0200

    Form email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 declare var bootstrap: any;  // Declaramos la variable bootstrap
@@ -9,11 +9,17 @@ declare function gtag_report_conversion(url: string): any; // Google Ads global
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'relief-pose-de-carrelage-et-sols-souples';
   showButton = false;
   constructor(private router: Router) {}
-
+  
+  ngOnInit(): void {
+      const urlInput = document.getElementById('form-url') as HTMLInputElement | null;
+      if (urlInput) {
+        urlInput.value = window.location.href;
+      }
+    }
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
